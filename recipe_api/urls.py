cbb5ff1089a683f6870283c2c78946b1b3b3bd65
Hyeungshik Jung <hyungshike@gmail.com>
2017-08-10 13:01:20 +0900

    Get recipes by group name

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^recipe/(\S+)', views.get_recipe),
+    url(r'^recipe/(?P<recipe_id>\w+)', views.get_recipe),
+    url(r'^dish/(?P<dish>\w+)', views.get_dish),
 ]
--- recipe_api/views.py
+++ recipe_api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404, get_list_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,9 +8,13 @@ from .serializers import RecipeSerializer
 
 @api_view(['GET'])
 def get_recipe(request, recipe_id):
-    try:
-        recipe = Recipe.objects.get(origin_id=recipe_id)
-    except Recipe.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    recipe = get_object_or_404(Recipe, origin_id=recipe_id)
     serializer = RecipeSerializer(recipe, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_dish(request, dish):
+    print(dish)
+    recipes = get_list_or_404(Recipe, group_name=dish)
+    serializer = RecipeSerializer(recipes, many=True)
     return Response(serializer.data)
