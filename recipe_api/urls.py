ec9ffb2a9f4a604e2591f46fb2af7ca649aa8d26
Hyeungshik Jung <hyungshike@gmail.com>
2017-08-10 20:45:00 +0900

    Make annotation into tree

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^recipe/(?P<recipe_id>\w+)', views.get_recipe),
     url(r'^recipes/(?P<dish>\w+)', views.get_recipes),
     url(r'^clusters/(?P<dish>\w+)', views.get_clusters),
+    url(r'^tree/(?P<recipe_id>\w+)', views.get_tree),
 ]
--- recipe_api/views.py
+++ recipe_api/views.py
@@ -1,10 +1,11 @@
+import json
 from django.shortcuts import get_object_or_404, get_list_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from recipe_api.models import Clustering
 from recipe_api.serializers import ClusteringSerializer, RecipeSerializer
-from tagger_api.models import Recipe
+from tagger_api.models import Recipe, Annotation
 
 
 @api_view(['GET'])
@@ -29,3 +30,43 @@ def get_clusters(request, dish):
     clusters = get_list_or_404(Clustering, dish_name=dish)
     serializer = ClusteringSerializer(clusters, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_tree(request, recipe_id):
+    """
+    :param request:
+    :param recipe_id: origin_id of Recipe model
+    :return: [(action, [ingredient])]
+    """
+    annotation = get_object_or_404(Annotation, recipe=recipe_id)
+    recipe = Recipe.objects.get(origin_id=recipe_id)
+    tags = annotation.annotations
+    instructions = recipe.instructions['instructions']
+    nodes = []
+    current_node = {"word": "", "ingredient": []}
+    for tag in tags:
+        if current_node["word"] == "" and tag['tag'] == 0:
+            current_node["word"] = get_word(instructions, tag['index'])
+        elif current_node["word"] != "" and tag['tag']== 0:
+            nodes.append(current_node)
+            current_node = {"word": "", "ingredient": []}
+        elif current_node["word"] == "" and tag['tag'] == 0:
+            pass
+        elif current_node["word"] != "" and tag['tag'] == 1:
+            current_node['ingredient'].append(get_word(instructions, tag['index']))
+    return nodes
+
+
+def get_word(instructions, index):
+    """
+    :param instruction: json object of instruction.
+    :param index: [int, int, int]
+    :return:
+    """
+    sentences  = instructions[index[0]]
+    tokens = sentences[index[1]]
+    word = tokens['tokens'][index[2]]['originalText']
+    return word.lower()
+
+
