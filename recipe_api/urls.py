a0f1f71d32f473a59a2ec757a87c82fb46035e41
Hyeungshik Jung <hyungshike@gmail.com>
2017-08-24 14:41:35 +0900

    Fix url match

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^clusters/(?P<dish>\w+)', views.get_clusters),
     url(r'^tree/(?P<recipe_id>\w+)', views.get_tree),
     url(r'^trees/(?P<dish>\w+)', views.get_trees),
-    url(r'^nodes', views.get_nodes_by_ids),
+    url(r'^nodes$', views.get_nodes_by_ids),
     url(r'^nodes/(?P<dish>\w+)', views.get_nodes),
     url(r'^histogram/(?P<dish>\w+)', views.get_action_ingredient_histogram),
     url(r'^histograms/(?P<dish>\w+)', views.get_histograms),
