b153f2448bad30a8a4ac4f54f45c0e64a25858ce
Hyeungshik Jung <hyungshike@gmail.com>
2017-08-29 22:23:55 +0900

    Add recipe & annotation to Django admin

--- tagger_api/admin.py
+++ tagger_api/admin.py
@@ -1,1 +1,9 @@
 # Register your models here.
+from django.contrib import admin
+from .models import Recipe, Annotation
+
+class RecipeAdmin(admin.ModelAdmin):
+    search_fields = ('title', )
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Annotation)
