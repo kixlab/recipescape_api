353538d1af4600220c45078bbf1356df9f32dbc3
Hyeungshik Jung <hyungshike@gmail.com>
2017-11-17 10:45:59 +0900

    Change cache to redis

--- recipescape_api/recipescape_api/settings.py
+++ recipescape_api/recipescape_api/settings.py
@@ -96,6 +96,19 @@ DATABASES = {
     }
 }
 
+# Caches
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
