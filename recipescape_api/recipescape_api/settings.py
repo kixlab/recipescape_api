c5285a1e406271d4e82771a9a0da6a6c5114bbca
Hyeungshik Jung <hyungshike@gmail.com>
2018-04-16 18:23:11 +0900

    Add database password

--- recipescape_api/recipescape_api/settings.py
+++ recipescape_api/recipescape_api/settings.py
@@ -94,6 +94,7 @@ DATABASES = {
         'USER': os.environ.get('PG_USER'),
         'HOST': os.environ.get('PG_HOST'),
         'PORT': os.environ.get('PG_PORT'),
+        'PASSWORD': os.environ.get('PG_PASSWORD'),
     }
 }
 
